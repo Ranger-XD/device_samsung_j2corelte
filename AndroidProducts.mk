6e69df67057a6009a498f1e0b82ba061e7016f28
Ranger-XD <singhshripal75@gmail.com>
2022-10-02 07:32:28 +0000

    j2corelte: lineage os bringup.
    added common lunch choices.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,1 +1,6 @@
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/omni_j2corelte.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/lineage_j2corelte.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_j2corelte-user \
+    lineage_j2corelte-userdebug \
+    lineage_j2corelte-eng
--- lineage_j2corelte.mk
+++ lineage_j2corelte.mk
@@ -3,14 +3,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_PACKAGES += \
 	charger_res_images \
 	charger
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := omni_j2corelte
+PRODUCT_NAME := lineage_j2corelte
 PRODUCT_DEVICE := j2corelte
 PRODUCT_MODEL := SM-J260F
 PRODUCT_BRAND := Samsung
